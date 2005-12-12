ad9ca91e7d42b5d33344d1332ae360c5acf1a1c2
erral <mlarreategi@codesyntax.com>
2005-12-12 16:43:40 +0000

    Falta ziren fitxategi batzuk
    svn path=/Bitakora/trunk/; revision=191

---
+++ ui/communityTemplates/logout.py
@@ -0,0 +1,10 @@
+## Script (Python) "logout"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+return container.cookie_authentication.logout()
