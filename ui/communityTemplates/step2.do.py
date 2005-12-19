32cac42a669b91ead7b433e1c9ed98820ec97df7
erral <mlarreategi@codesyntax.com>
2005-12-19 08:59:02 +0000

    Blogak sortzean id txarrak eta pasahitzen luzerak kontrolatu
    svn path=/Bitakora/trunk/; revision=49620

--- ui/communityTemplates/step2.do.py
+++ ui/communityTemplates/step2.do.py
@@ -10,10 +10,20 @@
 request = container.REQUEST
 RESPONSE =  request.RESPONSE
 
+ok_chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_ '
+def validId(id):
+    for letter in id:
+        if letter not in ok_chars:
+            return 0
+    return 1
+
 request.SESSION.set('title', request.form.get('title'))
 request.SESSION.set('id', request.form.get('id'))
 
 if container.get(request.form.get('id'), None) is not None:
-    return RESPONSE.redirect('%s/step2?r=4' % context.communityUrl())
+    return RESPONSE.redirect('%s/step2?r=1' % context.communityUrl())
+if not validId(request.form.get('id')):
+    return RESPONSE.redirect('%s/step2?r=2' % context.communityUrl())  
+
 
 return RESPONSE.redirect('%s/step3' % context.communityUrl())
